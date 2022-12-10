203f7324ce8b1f07b1c18ac52a9e237e2968d95e
Naitik <104677260+Booster1000@users.noreply.github.com>
2022-12-10 19:00:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+SpeechRecognition = window.webkitSpeechRecognition;
+//Web speech API//
+
+var Recognition = new SpeechRecognition();
+
+function start() {
+    document.getElementById('output').innerHTML = "";
+    Recognition.start();
+}
+
+Recognition.onresult = function (event) {
+    var content = event.results[0][0].transcript;
+
+    document.getElementById('output').innerHTML = content;
+    console.log(event);
+    if(content == 'take my selfie'){
+        speak();
+    }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;// Convert text to speech
+
+    speak_data = 'Taking Your Selfie In 5 seconds';
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+
+    timer();
+
+    setTimeout(function(){
+        takesnapshot();
+        save();
+      }, 13000);
+}
+
+camera = document.getElementById('camera');
+Webcam.set({
+    image_format : 'jpeg',
+    jpeg_quality : 100
+});
+
+function timer(){
+
+    var synth = window.speechSynthesis;// Convert text to speech
+
+    for(var i=5;i>=1;i--){
+
+    speak_data=i;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+    }
+
+    speak_data = 'Taking Your Selfie Now';
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+}
+
+function takesnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById('selfie').innerHTML = '<img id="s" src=" '+data_uri+' "> ';
+    });
+}
+
+function save(){
+    link = document.getElementById('link');
+    image = document.getElementById('s').src;
+    link.href = image;
+    link.click();
+}
